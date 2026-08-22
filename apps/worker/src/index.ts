ebb1d032d6d0482a55a24ebb4ef5f26986087b9f
Nivekithan <nivekithan363@gmail.com>
2026-08-22 17:11:57 +0530

    Create repo

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -2,6 +2,8 @@ import { Container, getContainer } from "@cloudflare/containers";
 import { env } from "cloudflare:workers";
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
+import * as archil from "disk";
+import z from "zod";
 
 export class GitContainer extends Container<Cloudflare.Env> {
   defaultPort = 3000;
@@ -10,31 +12,60 @@ export class GitContainer extends Container<Cloudflare.Env> {
 
 const app = new Hono<{ Bindings: Cloudflare.Env }>();
 
-app.all("/api", () => new Response("NOT FOUND", { status: 404 }));
+app.use(basicAuth({ password: env.GIT_PASSWORD, username: env.GIT_USERNAME }));
+
+/**
+ * Create a empty repo
+ */
+app.post("/api/repo", async (c) => {
+  const { repo, username } = z
+    .object({ username: z.string(), repo: z.string() })
+    .parse(c.body);
+
+  const repoKey = getRepoKey({ repo, username });
+
+  const [manageDisk, repoDisk] = await Promise.all([
+    archil.getDisk(c.env.ARCHIL_META_DISK),
+    archil.createDisk({ name: getDiskName({ repoKey }) }),
+  ]);
+
+  const token = repoDisk.token;
+
+  if (!token) throw new Error("Expected archil disk token to be returned");
+
+  /**
+   * Who needs a database when you have **DISK**
+   */
+  await manageDisk.putObject(repoKey, token);
+
+  return c.json({ ok: true });
+});
+
 app.all("/api/*", () => new Response("NOT FOUND", { status: 404 }));
 
-app.all(
-  "/:username/:repo/*",
-  basicAuth({
-    username: env.GIT_USERNAME,
-    password: env.GIT_PASSWORD,
-  }),
-  async (c) => {
-    const username = c.req.param("username");
-    const repo = c.req.param("repo");
+app.all("/:username/:repo/*", async (c) => {
+  const username = c.req.param("username");
+  const repo = c.req.param("repo");
 
-    const repository = repo.endsWith(".git") ? repo.slice(0, -4) : repo;
-    if (!repository) {
-      return c.notFound();
-    }
+  const repository = repo.endsWith(".git") ? repo.slice(0, -4) : repo;
+  if (!repository) {
+    return c.notFound();
+  }
 
-    const container = getContainer(
-      c.env.GIT_CONTAINER,
-      `${username}/${repository}`,
-    );
+  const container = getContainer(
+    c.env.GIT_CONTAINER,
+    `${username}/${repository}`,
+  );
 
-    return container.fetch(c.req.raw);
-  },
-);
+  return container.fetch(c.req.raw);
+});
+
+function getRepoKey({ repo, username }: { repo: string; username: string }) {
+  return `${repo}/${username}`;
+}
+
+function getDiskName({ repoKey }: { repoKey: string }) {
+  return `git-remote-${repoKey}`;
+}
 
 export default app;
