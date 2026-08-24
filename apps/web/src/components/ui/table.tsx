d2c7fd0e137708066a4e37c9acb63284f331f353
Nivekithan <nivekithan363@gmail.com>
2026-08-24 15:58:24 +0530

    UI update

--- apps/web/src/components/ui/table.tsx
+++ apps/web/src/components/ui/table.tsx
@@ -74,7 +74,7 @@ function TableRow<T>({ className, ...props }: RowProps<T>) {
     <RowPrimitive
       data-slot="table-row"
       className={cn(
-        "border-b transition-colors hover:bg-muted/50 has-aria-expanded:bg-muted/50 data-[state=selected]:bg-muted data-selected:bg-muted",
+        "h-11 border-b transition-colors hover:bg-muted/50 has-aria-expanded:bg-muted/50 data-[state=selected]:bg-muted data-selected:bg-muted",
         className
       )}
       {...props}
